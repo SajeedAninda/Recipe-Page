006d04380bf8fce940471c45769c12f082ae21df
SajeedAninda <sajaninda@gmail.com>
2024-06-22 15:26:06 +0600

    Created a layout for the page

---
+++ src/Components/Page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div className='w-full bg-[#fadceb] h-screen flex justify-center items-center'>
+            <div className="inner-div w-[60%] bg-white rounded-3xl h-fit p-8">
+                
+            </div>
+        </div>
+    );
+};
+
+export default Page;
