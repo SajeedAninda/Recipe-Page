91471da3ccc2db9e255a734c20c6d791290571e6
SajeedAninda <sajaninda@gmail.com>
2024-06-22 15:29:43 +0600

    Added the Image

--- src/Components/Image.jsx
+++ src/Components/Image.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import recipeImg from "../assets/mainImg.jpeg"
+
 const Image = () => {
     return (
         <div>
-            
+            <img className='rounded-2xl' src={recipeImg} alt="" />
         </div>
     );
 };
--- src/Components/Page.jsx
+++ src/Components/Page.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import Image from './Image';
 
 const Page = () => {
     return (
         <div className='w-full bg-[#fadceb] h-screen flex justify-center items-center'>
-            <div className="inner-div w-[60%] bg-white rounded-3xl h-fit p-8">
-                
+            <div className="inner-div w-[60%] bg-white rounded-2xl h-fit p-8">
+                <div className='imgDiv'>
+                    <Image></Image>
+                </div>
+
+                <div className='headerTextDiv'>
+                    
+                </div>
             </div>
         </div>
     );
